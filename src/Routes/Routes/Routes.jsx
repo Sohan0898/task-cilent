9bb37b7af85c7fed60bd3ed6b2e910faabb83151
sohan0898 <shuhanur.rahman08@gmail.com>
2023-12-21 01:03:40 +0600

    basic setup and set routes

---
+++ src/Routes/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../Layout/MainLayout/MainLayout";
+import Home from "../../Pages/Home/Home";
+
+
+
+
+const Routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+    //   errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element:<Home></Home>,
+        },
+      ],
+    },
+  ]);
+
+export default Routes;
